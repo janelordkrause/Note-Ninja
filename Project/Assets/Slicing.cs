23594532e694c1eb41203b44839688c266c390cd
Libby <LibbyB20@lakesideschool.org>
2020-03-15 15:46:48 -0700

    splitting blocks animation

--- Project/Assets/Slicing.cs
+++ Project/Assets/Slicing.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class Slicing : MonoBehaviour
 {
-    pubic GameObject Slices; //prefab of the two cut blocks
+    public GameObject Slices; //prefab of the two cut blocks
     
     //called when block is hit
     void OnTriggerEnter(Collider hit)
     {
         if (hit.tag == "Saber") //checks to make sure it was the saber that hit it
         {
-            Instantiate(Slices, transform.position, transform.rotation); //instantiates new split block object
+            Debug.Log(transform.position);
+            Instantiate(Slices, transform.position, transform.rotation); //instantiates new split block object, , transform.position, transform.rotation
             Destroy(gameObject); //destroys normal cube
         }
     }
