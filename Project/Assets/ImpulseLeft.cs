23cbeeb14c72576e75e2a346be4fd5bf51d5c533
Libby <LibbyB20@lakesideschool.org>
2020-03-17 17:59:35 -0700

    updated slicing graphics

---
+++ Project/Assets/ImpulseLeft.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpulseLeft : MonoBehaviour
+{
+	public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); //collects rigidbodies of both halves of split cube
+
+        //SOURCEhttps://learn.unity.com/tutorial/3d-physics#5c7f8528edbc2a002053b511
+        rb.AddForce(Vector3.left * 3, ForceMode.Impulse); //adds force on cubes
+        rb.AddForce(Vector3.up * 3, ForceMode.Impulse); //adds force on cubes
+        rb.AddForce(Vector3.back, ForceMode.Impulse); //adds force on cubes
+        Invoke("destroyGameObject", 10); //destroys slice after 10 seconds 
+    }
+
+    void destroyGameObject()
+    {
+    	Destroy(gameObject);
+    }
+}
+
--- Project/Assets/Slicing.cs
+++ Project/Assets/Slicing.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Slicing : MonoBehaviour
 {
     public GameObject Slices; //prefab of the two cut blocks
-    
+
     //called when block is hit
+    //SOURCEhttps://www.youtube.com/watch?v=3g5_8sE18tQ
     void OnTriggerEnter(Collider hit)
     {
         if (hit.tag == "Saber") //checks to make sure it was the saber that hit it
         {
-            Debug.Log(transform.position);
             Instantiate(Slices, transform.position, transform.rotation); //instantiates new split block object, , transform.position, transform.rotation
             Destroy(gameObject); //destroys normal cube
         }
