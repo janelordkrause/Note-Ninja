e5db0813c3634efff4fd7140f90c4e5e21cf74e9
Sophia Frank <sophiaf20@lakesideschool.org>
2020-04-28 09:33:04 -0700

    worked on spawning and timing issues

--- Project/Assets/Scripts/BlockSpawner.cs
+++ Project/Assets/Scripts/BlockSpawner.cs
@@ -22,7 +22,6 @@ public class BlockSpawner : MonoBehaviour
     private static int Y_ROTATION = 90;
     private static int Z_ROTATION = 0;
     public float beat; 
-    public bool startPlaying;
     public AudioSource music;
     private bool startSpawn;
     private bool songLength;
@@ -33,20 +32,17 @@ public class BlockSpawner : MonoBehaviour
     void Start()
     {
         beat = 60f/beat;
-        startPlaying = false;
 
-        reader = new StreamReader("Assets/Resources/sampleSong.txt");
+        //reader = new StreamReader("Assets/Resources/sampleSong.txt");
+        reader = new StreamReader("Assets/Resources/testRotation.txt");
     }
 
     void Update()
     {
         if (startSpawn == false)
         {
-            if (startPlaying == true)
-            {
-                spawns = StartCoroutine(SpawnBlock());
-                startSpawn = true;
-            }
+            spawns = StartCoroutine(SpawnBlock());
+            startSpawn = true;
         }
         if (music.isPlaying == false && startSpawn == true)
         {
@@ -70,13 +66,23 @@ public class BlockSpawner : MonoBehaviour
                 float xPos = float.Parse(blockInfo[1]); //x position is the second thing in the array
                 float yPos = float.Parse(blockInfo[2]);
                 Vector3 position = new Vector3(xPos, yPos, transform.position.z);
-                //int angleIndex = UnityEngine.Random.Range(0, 4); //never chooses the top num (glitch?)
-                //float xPos = UnityEngine.Random.Range(MIN_X, MAX_X);
-                //float yPos = UnityEngine.Random.Range(MIN_Y, MAX_Y);
                 Instantiate(block, position, Quaternion.Euler(rotation));
 
                 float wait = float.Parse(blockInfo[3]); //multiplier for wait time between blocks
                 yield return new WaitForSeconds(beat * wait);
             }
+            /*while (true) //spawns with random rotation and position
+            {
+            	int angleIndex = UnityEngine.Random.Range(0, 4); //never chooses the top num (glitch?)
+                Vector3 rotation = new Vector3(X_ROTATIONS[angleIndex], Y_ROTATION, Z_ROTATION);
+                block.gameObject.tag = X_TAGS[angleIndex]; //adds tag of direction they need to be sliced
+                
+                float xPos = UnityEngine.Random.Range(MIN_X, MAX_X);
+                float yPos = UnityEngine.Random.Range(MIN_Y, MAX_Y);
+                Vector3 position = new Vector3(xPos, yPos, transform.position.z);
+                Instantiate(block, position, Quaternion.Euler(rotation));
+
+                yield return new WaitForSeconds(beat);
+            }*/
     }
 }
