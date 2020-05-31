5bbfc22193fd31da0bce42844135564c86b161ef
Jane <janel20@lakesideschool.org>
2020-05-30 20:50:36 -0700

    working menu and song menu (except quit block)

---
+++ Project/Assets/Scripts/ButtonListener.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System; 
+using System.IO;
+
+
+public class ButtonListener : MonoBehaviour
+{
+	//GvrControllerInput controller; 
+    // Start is called before the first frame update
+    GameObject hit; 
+    string tag; 
+
+    void Start()
+    {
+     	   //controller = GetComponent<GvrControllerInput>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	HandleControllerInput();
+        
+    }
+
+    private void HandleControllerInput() {
+    	Debug.Log("method going");
+    	if (GvrControllerInput.ClickButton) {
+    		Debug.Log("clicked");
+			hit = GvrPointerInputModule.CurrentRaycastResult.gameObject; 
+			tag = hit.tag; 
+			try {
+				SceneManager.LoadScene(tag);
+			} catch (IOException e) {
+
+			}
+    		
+    	}
+    }
+/*
+    public override void OnPointerClick(PointerEventData data) {
+    	Debug.log("clicked");
+    }*/
+}
